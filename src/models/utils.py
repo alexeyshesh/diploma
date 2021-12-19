8a9e276cf1aec1cd444af3ebfadb668bebd7856c
alexeyshesh <alexeyshesh@yandex-team.ru>
2021-12-20 02:08:54 +0300

    flake-8

--- src/models/utils.py
+++
@@ -1,8 +0,0 @@
-import re
-from typing import Callable, Collection, List
-
-
-
-def get_by_ngrams(iterable: Collection, by: int = 2):
-    for i in range(by, len(iterable) + 1):
-        yield tuple(iterable[i-by:i])
